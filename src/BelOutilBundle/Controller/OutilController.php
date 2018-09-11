31054bf7ee3733d65e4aaccf323d1178c845a336
fbaconnais2018 <fbaconnais2018@12-UC29-17.ad.campus-eni.fr>
2018-09-11 14:48:33 +0200

    Préparation affichage articles dynamique

--- src/BelOutilBundle/Controller/OutilController.php
+++ src/BelOutilBundle/Controller/OutilController.php
@@ -4,11 +4,14 @@ namespace BelOutilBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class OutilController extends Controller
-{
+class OutilController extends Controller {
 
-    public function indexAction()
-    {
+    public function indexAction() {
+        $em = $this->getDoctrine()->getManager();
+        $repository = $em->getRepository('BelOutilBundle:Annonce');
 
+        $annonces = $repository->getNeufDern();
+        return $this->render('BelOutilBundle:Default:index.html.twig', array('annonces' => $annonces));
     }
+
 }

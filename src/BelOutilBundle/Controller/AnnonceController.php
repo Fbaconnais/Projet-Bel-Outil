2de1c57e7ff88d9c58a1a144b6c4353b58590c7a
rcorlosquet2018 <rcorlosquet2018@12-UC29-09.ad.campus-eni.fr>
2018-09-11 11:34:20 +0200

    création repository

---
+++ src/BelOutilBundle/Controller/AnnonceController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace BelOutilBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class AnnonceController extends Controller
+{
+
+    public function publicationAction()
+    {
+        
+    }
+}
+
